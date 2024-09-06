c7620e30e74cd79a934ec4fcd8b88c111672495e
Yasmamoto <yamamotoyato31@gmail.com>
2024-09-06 14:13:22 -0400

    Primeiro commit do AppGestaoCondominio

---
+++ AppGestaoCondominio/RegrasDeNegocio/Pessoa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppGestaoCondominio.RegrasDeNegocio
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public string Genero { get; set; }
+        public string DataNascimento { get; set; }
+        public string CPF { get; set; }
+        public double Renda { get; set; }
+       public string Titular(string titular)
+        {
+            if(titular == "S")
+            {
+                titular = "É PROPRIETARIO TITULAR";
+            }
+            else if(titular == "N")
+            {
+                titular = "NÃO É PROPRIETARIO TITULAR";
+            }
+            return titular;
+        }
+    }
+}
